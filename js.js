8377e1b9f4532f1fec42ea0113b43121ea6d51ce
henrique2601 <henrique_2601@hotmail.com>
2019-07-20 22:34:39 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+// Siema 1.3.0 introuduced a basic callbacks
+// https://github.com/pawelgrzybek/siema/releases/tag/v.1.3.0
+
+function onInitCallback() {
+  console.log('Siema initialised bro :)');
+}
+
+function onChangeCallback() {
+  console.log(`The index of current slide is: ${this.currentSlide}`);
+}
+
+const mySiema = new Siema({
+  onInit: onInitCallback,
+  onChange: onChangeCallback,
+});
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+prev.addEventListener('click', () => mySiema.prev());
+next.addEventListener('click', () => mySiema.next());
